7939b2f033f4cb4634fb6147d83cb49d11f80b55
Raymond Zheng <raymondzheng1994@gmail.com>
2024-03-19 20:14:46 -0400

    first commit

---
+++ lesson2.py
@@ -0,0 +1,26 @@
+place = input("Choose a place: forest/cave? ")
+
+if place == "forest":
+    action = input("climb a tree or cross a river?")
+    if action == "climb a tree":
+        print("you found a bird's nest")
+    if action == "cross a river":
+        print("you found a boat")
+else:
+    print("you found a hidden treasure")
+if place == "cave":
+    action = input("light a torch? or proceed in the dark?")
+    if action == "light a torch":
+        print("you are afraid of the dark")
+else:
+    print("you are one brave man")
+
+attendees = input("Enter number of attendees:")
+venue = ("large hall") if int(attendees) > 100 else ("conference room")
+venue = ("large hall") if int(attendees) > 100 else ("conference room") if int(attendees) < 100 else ("audio room")
+print(venue)
+food = input("do you want (vegan/meat)")
+if food == "vegan:":
+    print("i think you should try the Veggie Delight Caterers")
+else:
+    print("i would try Gourment Meals Caterers")
